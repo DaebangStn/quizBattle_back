7e8f9ae87583a8ec07b95959d919e856b1c78d44
DaebangStn <gunho1107@naver.com>
2022-06-08 09:33:23 +0000

    azure 1st deploy

--- quizBattle_back/settings.py
+++ quizBattle_back/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.leemworld.shop']
 
 
 # Application definition
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
